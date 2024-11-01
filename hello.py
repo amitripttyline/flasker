5d3f6cc6e6d1a3e92bcd5609ae46e6d2c5012bb3
Amit Bhardwaj <amitripttyline@gmail.com>
2024-11-02 04:56:07 +0530

    changes of forms before db

--- hello.py
+++ hello.py
@@ -1,8 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import pdb
+
 
 
 #create a flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "my_secret_key"
+
+# create a form class
+class NameForm(FlaskForm):
+ name = StringField("What's your name", validators=[DataRequired()])
+ submit = SubmitField("Submit")
 
 
 # create a route decorator
@@ -44,3 +55,30 @@ def page_not_gound(e):
 @app.errorhandler(500)
 def page_not_gound(e):
     return render_template("500.html"), 500
+
+# create name page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NameForm()
+
+    #debugging
+    # pdb.set_trace()  # Sets a breakpoint here
+
+    if request.method == 'POST':
+        print("Form submitted:", form.data)
+        print("Errors:", form.errors)
+    
+    # vaidate form
+    if form.validate_on_submit():
+        name = form.name.data 
+        form.name.data = ''
+        flash("Form submitted successfully")
+    else:
+        print("Form submitted but not valid.")
+        print("Errors:", form.errors)  # Check what errors are present
+        
+    return render_template("name.html", name=name, form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
